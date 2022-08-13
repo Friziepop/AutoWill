d22d135343825abbfb16a22862840eeb28f437b9
ldoron111@gmail.com <ldoron111@gmail.com>
2022-08-13 12:34:06 +0300

    aa

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
     optimizer.connect()
 
     constraints = [OptimizationConstraint(name='Res', max=120, min=80, start=100),
-                   OptimizationConstraint(name='QUARTER ', max=None, min=None, start=10)]
+                   OptimizationConstraint(name='QUARTER', max=None, min=None, start=10)]
     optimizer.setup(start_freq=4, end_freq=4.5, num_points=2, max_iter=10, optimization_type="Gradient Optimization",
                     constraints=constraints)
     optimizer.run_optimizer()
