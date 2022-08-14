85d8f0894c1f3941b943d55d9011cc60f926d0f4
Friziepop <shvmot@gmail.com>
2022-08-14 11:36:06 +0300

    added csv extraction to sparams

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import time
-from cgi import print_arguments
 import os
 import win32com.client as win32
 
@@ -33,7 +32,7 @@ if __name__ == '__main__':
     optimizer.setup(start_freq=4, end_freq=4.5, num_points=2, max_iter=10, optimization_type="Gradient Optimization",
                     constraints=constraints)
     optimizer.run_optimizer()
-    res = extractor.extract_results()
+    res = extractor.extract_results(4.0, 3)
     x=5
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
