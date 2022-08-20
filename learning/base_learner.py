d8b61ee1999c119ffbc4fb37d89abc6cd3c54906
ldoron111@gmail.com <ldoron111@gmail.com>
2022-08-20 13:49:16 +0300

    refactor learners

--- learning/base_learner.py
+++ learning/base_learner.py
@@ -1,3 +1,4 @@
+from abc import ABC
 from typing import List
 
 from sklearn.linear_model import LinearRegression
@@ -8,10 +9,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-class LinearLearner:
-
+class BaseLearner(ABC):
     def __init__(self):
-        self.degree = 1
+        pass
 
     def extract_xy(self, csv_data_path: str, x_col: str, y_col: str):
         df = pd.read_csv(filepath_or_buffer=csv_data_path, dtype={x_col: float, y_col: float})
@@ -20,26 +20,25 @@ class LinearLearner:
         return x, y
 
     def create_feature(self, x):
-        bias_ones = np.ones(len(x))
-        tmp = np.array([bias_ones, 1 / x])
-        return tmp.T
+        pass
 
     def get_calculated_y(self, x, coefficients: List[float]):
         calculated_features = self.create_feature(x)
         return [row @ coefficients for row in calculated_features]
 
-    def draw_graph(self, csv_data_path: str, x_col: str, y_col: str, coefficients: List[float]):
+    def draw_graph(self, csv_data_path: str, title: str, x_col: str, y_col: str, coefficients: List[float]):
         x, y = self.extract_xy(csv_data_path, x_col, y_col)
         calculated_y = self.get_calculated_y(x=x, coefficients=coefficients)
         # plot
         fig, ax = plt.subplots()
-        ax.scatter(x, y,color='blue')
-        ax.plot(x, calculated_y,color='red')
+        ax.scatter(x, y, color='blue')
+        ax.plot(x, calculated_y, color='red')
 
         ax.set(xlim=(0, 20), xticks=np.arange(1, 30),
                ylim=(0, 20), yticks=np.arange(1, 50))
         ax.set_ylabel(y_col)
         ax.set_xlabel(x_col)
+        ax.set_title(title)
 
         plt.show()
 
@@ -56,6 +55,3 @@ class LinearLearner:
         return model.coef_
 
 
-learner = LinearLearner()
-coeff = learner.train("../vars.csv", x_col="frequency", y_col="QUARTER")
-learner.draw_graph("../vars.csv", x_col="frequency", y_col="QUARTER", coefficients=coeff)
---
+++ learning/learners.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+from learning.base_learner import BaseLearner
+
+
+class InverseLearner(BaseLearner):
+    def create_feature(self, x):
+        bias_ones = np.ones(len(x))
+        tmp = np.array([bias_ones, 1 / x])
+        return tmp.T
+
+
+class PolyLearner(BaseLearner):
+    def __init__(self, degree: int):
+        super().__init__()
+        self._degree = degree
+
+    def create_feature(self, x):
+        bias_ones = np.ones(len(x))
+        tmp = np.array([bias_ones])
+        for deg in range(1, self._degree + 1):
+            tmp = np.vstack((tmp, np.power(x,deg)))
+
+        return tmp.T
