2389d117063187dccf206328a3f51f44ea0074bf
ldoron111@gmail.com <ldoron111@gmail.com>
2022-08-21 15:52:52 +0300

    new material dict

--- microstip_freq_calc/microstrip_freq_automation_extractor.py
+++ microstip_freq_calc/microstrip_freq_automation_extractor.py
@@ -1,41 +1,48 @@
 import math
 import pickle
 from time import sleep
+from typing import Dict
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from tqdm import tqdm
 
+fr4_params = {
+    "prefix":"fr4","er": 4.4, "tanl": 0.018, "rho": 0.7, "height": 60, "thickness": 1.377952755905512, "z0": 50
+}
+ro4350_params = {
+        "prefix": "ro4350","er": 3.66, "tanl": 0.0037, "rho": 0.7, "height": 3.93701, "thickness": 0.039370079, "z0": 50
+}
 
-def main():
+def extract(config_dict: Dict):
     # all unitr are in mills
     dict_ans = {}
-    dict_ans_root={}
+    dict_ans_root = {}
     freqs = [round(0.1 * x, 1) for x in range(0, 1000)]
     driver = webdriver.Chrome('./chromedriver')
     driver.get("https://www.microwaves101.com/calculators/1201-microstrip-calculator")
     sleep(2)
     er = driver.find_element(value="edt_msEr")
     er.clear()
-    er.send_keys(4.4)
+    er.send_keys(config_dict["er"])
     tanl = driver.find_element(value="edt_msTand")
     tanl.clear()
-    tanl.send_keys(0.018)
+    tanl.send_keys(config_dict["tanl"])
     rho = driver.find_element(value="edt_msRho")
     rho.clear()
-    rho.send_keys(0.7)
+    rho.send_keys(config_dict["rho"])
     freq_form = driver.find_element(value="edt_msFreq")
 
     height = driver.find_element(value="edt_msHeight")
     height.clear()
-    height.send_keys(60)
-    tanl = driver.find_element(value="edt_msThickness")
-    tanl.clear()
-    tanl.send_keys(1.377952755905512)
+    height.send_keys(config_dict["height"])
+    thickness = driver.find_element(value="edt_msThickness")
+    thickness.clear()
+    thickness.send_keys(config_dict["thickness"])
 
     zo = driver.find_element(value="edt_msZo")
     zo.clear()
-    zo.send_keys(50)
+    zo.send_keys(config_dict["z0"])
     angle = driver.find_element(value="edt_msAngle")
     angle.clear()
     angle.send_keys(90)
@@ -51,13 +58,13 @@ def main():
         print(f"{freq}:{val}")
         dict_ans[f"{freq}"] = val
 
-    with open('freq2width_dict.pickle', 'wb') as handle:
+    with open(f'{config_dict["prefix"]}_freq2width_dict.pickle', 'wb') as handle:
         pickle.dump(dict_ans, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-    #root
+    # root
     print("now root")
     zo.clear()
-    zo.send_keys(50 * math.sqrt(2))
+    zo.send_keys(config_dict["z0"] * math.sqrt(2))
     angle.clear()
     angle.send_keys(180)
 
@@ -70,13 +77,9 @@ def main():
         print(f"{freq}:{val}")
         dict_ans[f"{freq}"] = val
 
-    with open('fr4_freq2width_root_dict.pickle', 'wb') as handle:
+    with open(f'{config_dict["prefix"]}_freq2width_root_dict.pickle', 'wb') as handle:
         pickle.dump(dict_ans_root, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 if __name__ == '__main__':
-    #main()
-    with open('fr4_freq2width_root_dict.pickle', 'rb') as handle:
-        b_dict = pickle.load(handle)
-        for key, val in b_dict.items():
-            print(f"{key}:{val}")
+    extract(config_dict=ro4350_params)
