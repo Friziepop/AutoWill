e7824f089f758af17fde840967eedab3b09a7131
ldoron111@gmail.com <ldoron111@gmail.com>
2022-08-24 16:08:38 +0300

    finished generating script

--- microstip_freq_calc/microstrip_freq_automation_extractor.py
+++ microstip_freq_calc/microstrip_freq_automation_extractor.py
@@ -3,22 +3,28 @@ import pickle
 from time import sleep
 from typing import Dict
 
+import numpy as np
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from tqdm import tqdm
 
-fr4_params = {
-    "prefix":"fr4","er": 4.4, "tanl": 0.018, "rho": 0.7, "height": 60, "thickness": 1.377952755905512, "z0": 50
-}
-ro4350_params = {
-        "prefix": "ro4350","er": 3.66, "tanl": 0.0037, "rho": 0.7, "height": 3.93701, "thickness": 0.039370079, "z0": 50
-}
+# fr4_params = {
+#     "prefix":"fr4","er": 4.4, "tanl": 0.018, "rho": 0.7, "height": 60, "thickness": 1.377952755905512, "z0": 50
+# }
+# ro4350_params = {
+#         "prefix": "ro4350","er": 3.66, "tanl": 0.0037, "rho": 0.7, "height": 3.93701, "thickness": 0.039370079, "z0": 50
+# }
 
-def extract(config_dict: Dict):
-    # all unitr are in mills
+Z0 = 50
+MILLS_TO_MM = 0.0254
+MM_TO_MILS = 1 / MILLS_TO_MM
+
+
+def extract(config_dict: Dict, step_size):
     dict_ans = {}
     dict_ans_root = {}
-    freqs = [round(0.1 * x, 1) for x in range(0, 1000)]
+    freqs = [round(x, 1) for x in np.arange(config_dict["start_freq"], config_dict["end_freq"] + step_size, step_size)]
     driver = webdriver.Chrome('./chromedriver')
     driver.get("https://www.microwaves101.com/calculators/1201-microstrip-calculator")
     sleep(2)
@@ -35,36 +41,35 @@ def extract(config_dict: Dict):
 
     height = driver.find_element(value="edt_msHeight")
     height.clear()
-    height.send_keys(config_dict["height"])
+    height.send_keys(config_dict["height"] * MM_TO_MILS)
     thickness = driver.find_element(value="edt_msThickness")
     thickness.clear()
-    thickness.send_keys(config_dict["thickness"])
+    thickness.send_keys(config_dict["thickness"] * MM_TO_MILS)
 
     zo = driver.find_element(value="edt_msZo")
     zo.clear()
-    zo.send_keys(config_dict["z0"])
+    zo.send_keys(Z0)
     angle = driver.find_element(value="edt_msAngle")
     angle.clear()
     angle.send_keys(90)
 
     btn_sy = driver.find_element(value="btn_ms_synthesize")
     width = driver.find_element(value="edt_msWidth")
-    # # for freq in tqdm(freqs):
-    # #     freq_form.clear()
-    # #     freq_form.send_keys(freq)
-    # #     btn_sy.click()
-    # #     sleep(0.1)
-    # #     val = float(width.get_attribute("value")) * 0.0254
-    # #     print(f"{freq}:{val}")
-    # #     dict_ans[f"{freq}"] = val
-    #
-    # with open(f'{config_dict["prefix"]}_freq2width_dict.pickle', 'wb') as handle:
-    #     pickle.dump(dict_ans, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    for freq in tqdm(freqs):
+        freq_form.clear()
+        freq_form.send_keys(freq)
+        btn_sy.click()
+        sleep(0.1)
+        val = float(width.get_attribute("value")) * MILLS_TO_MM
+        dict_ans[f"{freq}"] = val
+
+    with open(f'{config_dict["name"]}_freq2width_dict.pickle', 'wb') as handle:
+        pickle.dump(dict_ans, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
     # root
     print("now root")
     zo.clear()
-    zo.send_keys(config_dict["z0"] * math.sqrt(2))
+    zo.send_keys(Z0 * math.sqrt(2))
     angle.clear()
     angle.send_keys(180)
 
@@ -73,13 +78,15 @@ def extract(config_dict: Dict):
         freq_form.send_keys(freq)
         btn_sy.click()
         sleep(0.1)
-        val = float(width.get_attribute("value")) * 0.0254
-        print(f"{freq}:{val}")
+        val = float(width.get_attribute("value")) * MILLS_TO_MM
         dict_ans_root[f"{freq}"] = val
 
-    with open(f'{config_dict["prefix"]}_freq2width_root_dict.pickle', 'wb') as handle:
+    with open(f'{config_dict["name"]}_freq2width_root_dict.pickle', 'wb') as handle:
         pickle.dump(dict_ans_root, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 if __name__ == '__main__':
-    extract(config_dict=ro4350_params)
+    materials_csv = pd.read_csv('../materials/materials_db.csv')
+    for config_dict in materials_csv.to_dict('records'):
+        print(f"starting material :{config_dict['name']}")
+        extract(config_dict=config_dict, step_size=0.1)
