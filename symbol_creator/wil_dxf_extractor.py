1471f50b665c97947b566e18602269d835d8ed62
lavi <ldoron111@gmail.com>
2022-09-30 14:58:53 +0300

    added dxf extractor

---
+++ symbol_creator/wil_dxf_extractor.py
@@ -0,0 +1,18 @@
+from typing import Tuple
+
+import ezdxf
+
+
+class WilDxfExtractor:
+    def __init__(self, wil_layout_file_path: str):
+        self._doc = ezdxf.readfile(wil_layout_file_path)
+
+    def extract_layout_angle_mid(self, extract_top=True) -> Tuple[float, float]:
+        angle_ent = [ent for ent in list(self._doc.entities) if len(ent.vertices) == 4]
+        if extract_top:
+            chosen_ent = [ent for ent in angle_ent if ent.vertices[0] > 0][0]
+        else:
+            chosen_ent = [ent for ent in angle_ent if ent.vertices[0] < 0][0]
+
+        return (chosen_ent.vertices[1].x + chosen_ent.vertices[2].x) / 2, (
+                    chosen_ent.vertices[1].y + chosen_ent.vertices[2].y) / 2
