1116786787566335a0e495e574fa1f1e34d62d2f
Tom Shvartzman <shvmot@gmail.com>
2023-01-01 23:47:50 +0200

    gui fixes

--- symbol_creator/gui_main.py
+++ symbol_creator/gui_main.py
@@ -1,5 +1,6 @@
 import shutil
 import time
+from threading import Thread
 from typing import List
 
 import PySimpleGUI as sg
@@ -16,6 +17,11 @@ def copy_files(pad_name: str, dst_path: str):
     shutil.copy2(src=f"../orcad/package/wil_sym.psm", dst=dst_path)
 
 
+def run_script(mat_id: int, freq: float, er: float, tanl: float):
+    SymbolCreator().create(material_id=mat_id, frequency=freq,
+                           er=er, tanl=tanl)
+
+
 material_db = MaterialDB(MATERIALS_DB)
 materials: List[Material] = material_db.get_all()
 
@@ -24,20 +30,22 @@ materials_names = list(set([mat.name for mat in materials]))
 
 resistor_names = all_resistors_names
 
+sg.theme("Dark")
 # Define the window's contents
-layout = [[sg.Text("output")],
-          [sg.Text("Enter frequency [GHZ]"), sg.Input(key='-FREQUENCY-')],
-          [sg.Text("Enter material"),
-           sg.Combo(materials_names, key='-MATERIAL_NAME-', readonly=True, change_submits=True)],
-          [sg.Text("Enter resistor"), sg.Combo(resistor_names, key='-RESISTOR_NAME-', readonly=True)],
-          [sg.Text("Enter e_r"), [sg.Input(key='-E_R-')]],
-          [sg.Text("Enter tanl"), sg.Input(key='-TANL-')],
-          [sg.Text('Select a folder:'), sg.FolderBrowse(key='-DST_FOLDER-')],
-          [sg.Output(size=(50, 10), key='-OUTPUT-')],
-          [sg.Button('Ok')]]
+layout = [
+    [sg.Text("Enter frequency [GHZ]"), sg.Input(key='-FREQUENCY-')],
+    [sg.Text("Enter material"),
+     sg.Combo(materials_names, key='-MATERIAL_NAME-', readonly=True, change_submits=True)],
+    [sg.Text("Enter resistor"), sg.Combo(resistor_names, key='-RESISTOR_NAME-', readonly=True)],
+    [sg.Text("Enter e_r"), [sg.Input(key='-E_R-')]],
+    [sg.Text("Enter tanl"), sg.Input(key='-TANL-')],
+    [sg.Text('Select a folder:'), sg.In(key='-DST_FOLDER-'), sg.FolderBrowse("Select")],
+    [sg.Multiline(size=(50, 10), key='-OUTPUT-', reroute_stdout=True), sg.StatusBar("done", background_color="green",text_color="black",key="-STATUS_BAR-")],
+    [sg.Button('Ok')]]
 
 # Create the window
 window = sg.Window('Symbol creator', layout)
+ryn_script_thread = Thread(target=run_script, args=window)
 
 # Display and interact with the Window using an Event Loop
 while True:
@@ -50,7 +58,7 @@ while True:
         resistor_names = [mat.resistor.name for mat in materials if mat.name == window['-MATERIAL_NAME-'].get()]
         res_combo.update(value=res_combo.get() if res_combo.get() in resistor_names else resistor_names[0],
                          values=resistor_names)
-    if event == 'Ok':
+    if event == 'Ok' and not ryn_script_thread.is_alive():
         possible_matches = [mat for mat in materials if
                             mat.name == window['-MATERIAL_NAME-'].get() and mat.resistor.name == window[
                                 '-RESISTOR_NAME-'].get()]
@@ -61,14 +69,17 @@ while True:
         print(
             f"chose frequency : {window['-FREQUENCY-'].get()},e_r:{window['-E_R-'].get()} ,tanl:{window['-TANL-'].get()} , material id : {selected_mat.id} , destination folder :{window['-DST_FOLDER-'].get()}")
         try:
-            SymbolCreator().create(material_id=selected_mat.id, frequency=float(window['-FREQUENCY-'].get()),
-                                   er=float(window['-E_R-'].get()), tanl=float(window['-TANL-'].get()))
+            ryn_script_thread = Thread(target=run_script, args=(selected_mat.id,float(window['-FREQUENCY-'].get()),float(window['-E_R-'].get()),float(window['-TANL-'].get())))
+            window['-STATUS_BAR-'].update(value="running",background_color="yellow")
+            ryn_script_thread.start()
             dst_folder = window["-DST_FOLDER-"].get()
             copy_files(pad_name=selected_mat.resistor.pad_name, dst_path=dst_folder)
-            window.refresh()
-            break
+            # window.refresh()
         except Exception as e:
             print(f"error in symbol creator : {e}")
+    if not ryn_script_thread.is_alive():
+        window['-STATUS_BAR-'].update(value="done", background_color="green",text_color="black")
+    window.refresh()
 
 # Finish up by removing from the screen
 window.close()
